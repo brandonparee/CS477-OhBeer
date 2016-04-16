02554e158ce1af9bf5faa18ef60a19929294c6bd
jrf254 <jrf254@nau.edu>
2016-04-16 14:22:46 -0700

    Fixed the auto menu update

--- server/publications/index.js
+++ server/publications/index.js
@@ -9,7 +9,5 @@ Meteor.publish( 'myBar', function() {
 Meteor.publish( 'myMenu', function(barId) {
   check(barId, String);
   let myId = Bars.findOne(barId);
-  console.log(myId);
-  console.log(myId.menu);
-  return MenuItems.find({_id: {$in:myId.menu}})
+  return MenuItems.find({barId: myId._id});
 })
