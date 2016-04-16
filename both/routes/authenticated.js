474e1fe51597d5b723d699f9855ab72ed29b79c0
jrf254 <jrf254@nau.edu>
2016-04-16 15:21:04 -0700

    Fixed the index page

--- both/routes/authenticated.js
+++ both/routes/authenticated.js
@@ -3,13 +3,6 @@ const authenticatedRoutes = FlowRouter.group( { name: 'authenticated' } );
 authenticatedRoutes.route( '/', {
   name: 'index',
   action() {
-    BlazeLayout.render( 'default', { yield: 'index' } );
-  }
-});
-
-authenticatedRoutes.route( '/dashboard', {
-  name: 'dashboard',
-  action() {
     BlazeLayout.render( 'default', { yield: 'dashboard' } );
   }
 });
