029e0df935ce09f30e132315c73c90b8406de866
jrf254 <jrf254@nau.edu>
2016-04-16 20:33:52 -0700

    Added lat and lon on bar creation and update

--- server/methods/methods.js
+++ server/methods/methods.js
@@ -1,8 +1,16 @@
+import { EJSON } from 'meteor/ejson'
+
 Meteor.methods({
   'setLatLon': function(barId){
     check(barId, String);
     let bar = Bars.findOne(barId);
-    console.log(bar);
-    console.log("GET", "https://maps.googleapis.com/maps/api/geocode/json?address=" + bar.location.address + bar.location.city + bar.location.state + bar.location.zip + "&key=AIzaSyBrek7eusc5FCeBAYatUTYuX41dSmWw8NQ");
+    //console.log(bar);
+    //console.log("https://maps.googleapis.com/maps/api/geocode/json?address=" + bar.location.address + bar.location.city + bar.location.state + bar.location.zip + "&key=AIzaSyBrek7eusc5FCeBAYatUTYuX41dSmWw8NQ");
+    let results = HTTP.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + bar.location.address + bar.location.city + bar.location.state + bar.location.zip + "&key=AIzaSyBrek7eusc5FCeBAYatUTYuX41dSmWw8NQ");
+    console.log(results.data.results[0].geometry.location.lat);
+    //console.log(results.data.geometry.location.lat);
+    //console.log(results.data.geometry.location.lng);
+    Bars.update(barId, {$set:{'location.lat': results.data.results[0].geometry.location.lat}});
+    Bars.update(barId, {$set:{'location.lon': results.data.results[0].geometry.location.lng}});
   }
 });
