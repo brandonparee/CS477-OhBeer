79199d3ea3a4141b1ea8996fa33e6aa6c7adc7ba
Brandon Paree <brandon.paree@gmail.com>
2016-04-16 17:34:15 -0700

    Added subscription for users on mobile.

--- server/publications/template.js
+++ server/publications/template.js
@@ -1,3 +1,7 @@
 Meteor.publish( 'template', function() {
   return Collection.find( { 'owner': this.userId }, { fields: { 'owner': 1 } } );
 });
+
+Meteor.publish('userProfile', function() {
+  return Meteor.users.find(this.user)
+})
