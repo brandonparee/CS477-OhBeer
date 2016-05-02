cf9c85242f7e7f30183ad52414c28810bc2bb008
Brandon Paree <brandon.paree@gmail.com>
2016-05-02 12:28:34 -0700

    Added more browser policies and keevan's image.

--- server/modules/set-browser-policies.js
+++ server/modules/set-browser-policies.js
@@ -1,5 +1,11 @@
 export default function() {
   //BrowserPolicy.content.allowOriginForAll('*');
+  BrowserPolicy.content.allowOriginForAll('blob:');
+  BrowserPolicy.content.allowOriginForAll('*.amazonaws.com')
+
+
   BrowserPolicy.content.allowInlineStyles()
   BrowserPolicy.content.allowFontDataUrl()
+  BrowserPolicy.content.allowSameOriginForAll();
+
 }
